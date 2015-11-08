eeb8096b159b4d8be64cae18cd89a327a8941a54
WONG KEAT HONG <Raydivine@users.noreply.github.com>
2015-11-08 15:10:51 +0800

    Delete InitNode.h

--- test/support/InitNode.h
+++
@@ -1,9 +0,0 @@
-#ifndef InitNode_H
-#define InitNode_H
-#include "Node.h"
-
-void setNode(Node *target, Node *left, Node *right, char color);
-void resetNode(Node *target, int data);
-
-
-#endif // InitNode_H
