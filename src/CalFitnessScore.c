70471e096690fef8a6c4820ff76553e8232c0331
Raydivine <raydivine00@gmail.com>
2015-11-19 04:49:00 +0800

    solving element mising problem of calConflictFromPaperToPaparList()

--- src/CalFitnessScore.c
+++ src/CalFitnessScore.c
@@ -1,6 +1,7 @@
 #include "CalFitnessScore.h"
 #include "ExamStruct.h"
 #include "LinkedList.h"
+#include <stdio.h>
 
 void calFitnessScore(Session *session){
   int fitnessScore =0;
@@ -9,11 +10,37 @@ void calFitnessScore(Session *session){
   session->fitnessScore =0;
 }
 
+
+void printProgrammeList( LinkedList **programmeList){
+  LinkedList *list1 = *programmeList;
+  while( list1 != NULL){
+    printf("%p\n",list1->data);
+    list1 = list1->next;
+  }
+}
+
+
 int calConflictFromPaperToPaparList(Paper *paper, LinkedList *paperList){
+  Paper *paperX;
   int conflict =0;
   
-  LinkedList *programmeList1 = paper->programmes;
+  LinkedList *programmeList1 = paper->programmes, *programmeList2;
+
+  conflict = conflict + isListsHaveSameElement(programmeList1, programmeList1);
   
+  // while(paperList != NULL){
+    // paperX = (Paper*)paperList->data;
+    // // paperX = (Paper*)paperList->data;
+    // programmeList2 = paperX->programmes;
+    // printProgrammeList( programmeList2);;
+    // printf("\n");
+ 
+    
+    // printf("in\n");
+    // conflict = conflict + isListsHaveSameElement(programmeList1, programmeList2);
+    // paperList = paperList->next;
+  // }
   
   return conflict;
 }
+
--- test/test_calConflictFromPaperToPaparList.c
+++ test/test_calConflictFromPaperToPaparList.c
@@ -5,35 +5,38 @@
 #include "LinkedListAdd.h"
 #include "SetElements.h"
 
+//C:\Users\Raydivine\Desktop\ExamTimeTabling\build\test\out\test_calConflictFromPaperToPaparList.exe
+
+
 Paper p1,p2,p3,p4;
 Programme c1,c2,c3,c4,c5,c6,c7,c8;
 
+LinkedList *testersP1, *testersP2, *testersP3;
 LinkedList *paperList;
 
-void setUp(void)
-{
-  LinkedList *testersP1 = linkListNew(&c1);
+void setUp(void){
+  
+  testersP1 = linkListNew(&c1);
   addDataToHead(&testersP1, &c2);
   setPaper(&p1, "p1", &testersP1);
   
-  LinkedList *testersP2 = linkListNew(&c3);
+  testersP2 = linkListNew(&c3);
   addDataToHead(&testersP2, &c4);
   setPaper(&p2, "p2", &testersP2);
   
-  LinkedList *testersP3 = linkListNew(&c4);
+  testersP3 = linkListNew(&c4);
   addDataToHead(&testersP3, &c5);
   setPaper(&p3, "p3", &testersP3);
   
-  LinkedList *paperList = linkListNew(&p1);
+  paperList = linkListNew(&p1);
   addDataToHead(&paperList, &p2);
   addDataToHead(&paperList, &p3);
   
-  /**
-  *                               PaperList
-  * Papers                  :    p1              p2              p3
-  * programmes taking       :  c1,c2            c3,c4          c5,c6
-  *
-  */
+ /**
+ *                                 PaperList
+ * Papers                  :   p1              p2              p3
+ * programmes taking       : c1,c2            c3,c4          c5,c6
+*/
 }
 
 void tearDown(void){}
@@ -45,30 +48,49 @@ void tearDown(void){}
  *
  *                               (no conflict)
 */
-void test_calConflictFromPaperToPaparList_given_p4_takers_are_c7_c8_which_no_confict_with_paperList_should_get0(void){
-  LinkedList *testersP4 = linkListNew(&c7);
-  addDataToHead(&testersP4, &c8);
-  setPaper(&p4, "p4", &testersP4);
+// void xtest_calConflictFromPaperToPaparList_given_p4_takers_are_c7_c8_which_no_confict_with_paperList_should_get0(void){
   
-  int ans = calConflictFromPaperToPaparList( &p4, paperList);
-  TEST_ASSERT_EQUAL(0, ans);
+  // LinkedList *testersP4 = linkListNew(&c7);
+  // addDataToHead(&testersP4, &c8);
+  // setPaper(&p4, "p4", &testersP4);
   
-  clearLinkList(&testersP4);
-}
+  // int ans = calConflictFromPaperToPaparList( &p4, paperList);
+  // TEST_ASSERT_EQUAL(0, ans);
+  
+  
+  // clearLinkList(&testersP4);
+// }
+
 
 /**
  *                             Paper            PaperList
  * Papers                  :    p4     |          p1              p2              p3
- * programmes taking       :  c1,c2    |       c1,c2            c3,c4          c5,c6
+ * programmes taking       :  c1,c8    |       c1,c2            c3,c4          c5,c6
  *
- *                               (no conflict)
+ *                               (1 conflict)
 */
-// void xtest_calConflictFromPaperToPaparList_given_p4_takers_are_c1_c8_which_have_1_confict_with_paperList_should_get1(void){
-  // LinkedList *testersP4 = linkListNew(&c1);
-  // addDataToHead(&testersP4, &c2);
-  // setPaper(&p4, "p4", &testersP4);
+void test_calConflictFromPaperToPaparList_given_p4_takers_are_c1_c8_have_1_confict_with_paperList_should_get0(void){
   
-  // int conflict = calConflictFromPaperToPaparList( &p4, paperList);
-  // TEXT_ASSERT_EQUAL(1, conflict);
+  LinkedList *testersP4 = linkListNew(&c1);
+  addDataToHead(&testersP4, &c8);
+  setPaper(&p4, "p4", &testersP4);
   
-// }
+  int ans = calConflictFromPaperToPaparList( &p4, paperList);
+  TEST_ASSERT_EQUAL(1, ans);
+   
+  clearLinkList(&testersP4);
+}
+
+
+
+void xtest_fdsfsdsd(void){
+  
+  LinkedList *testersP4 = linkListNew(&c1);
+  addDataToHead(&testersP4, &c5);
+  
+  LinkedList *testersP5 = linkListNew(&c3);
+  addDataToHead(&testersP5, &c5);
+  
+  int ans = isListsHaveSameElement(testersP4, testersP5);
+  TEST_ASSERT_EQUAL(1, ans); 
+}
