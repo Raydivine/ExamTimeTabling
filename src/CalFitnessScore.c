9c7c45e4e8ab869e611429748fad2911e38e0cb3
Raydivine <raydivine00@gmail.com>
2015-11-19 08:47:23 +0800

    5 test for calFitnessScore()

--- src/CalFitnessScore.c
+++ src/CalFitnessScore.c
@@ -5,20 +5,17 @@
 
 void calFitnessScore(Session *session){
   int fitnessScore =0;
+  Paper *paperX;
+  LinkedList *paperList = session->papers;
   
-  
-  session->fitnessScore =0;
-}
-
-
-void printProgrammeList( LinkedList **programmeList){
-  LinkedList *list1 = *programmeList;
-  while( list1 != NULL){
-    printf("%p\n",list1->data);
-    list1 = list1->next;
+  while(paperList !=NULL){
+    paperX = (Paper*)paperList->data;                                                       //1. TypeClass the paperList->data to paperX
+    fitnessScore = fitnessScore + calConflictFromPaperToPaparList(paperX, paperList->next); //2. calculate conflict between paperX and the paperList->next,continue add 
+    paperList = paperList->next;
   }
+  
+  session->fitnessScore =fitnessScore;
 }
-
 
 int calConflictFromPaperToPaparList(Paper *paper, LinkedList *paperList){
   int conflict =0;
@@ -26,9 +23,9 @@ int calConflictFromPaperToPaparList(Paper *paper, LinkedList *paperList){
   LinkedList *programmeList1 = paper->programmes, *programmeList2;
 
   while(paperList != NULL){
-    paperX = (Paper*)paperList->data;
-    programmeList2 = paperX->programmes;
-    conflict = conflict + isListsHaveSameElement(programmeList1, programmeList2);
+    paperX = (Paper*)paperList->data;                                             //1. TypeClass the paperList->data to paperX
+    programmeList2 = paperX->programmes;                                          //2. take out programmeList from paperX's 
+    conflict = conflict + isListsHaveSameElement(programmeList1, programmeList2); //3. check is the 2 list have same element,continue add 
     paperList = paperList->next;
   }
   
--- test/test_CalFitnessScore.c
+++ test/test_CalFitnessScore.c
@@ -6,61 +6,140 @@
 #include "SetElements.h"
 
 Paper p1,p2,p3,p4,p5,p6;
-Programme c1,c2,c3,c4,c5,c6;
+Programme c1,c2,c3,c4,c5,c6,c7;
 
 Session session;
 
 void setUp(void)
 {
-  LinkedList *testersP1 = linkListNew(&c1);
-  addDataToHead(&testersP1, &c2);
-  setPaper(&p1, "p1", &testersP1);
+//-----------------TEST ELEMENT----------SHARE TO ALL TEST  
+  LinkedList *takersP1 = linkListNew(&c1);
+  addDataToHead(&takersP1, &c2);
+  setPaper(&p1, "p1", takersP1);
   
-  LinkedList *testersP2 = linkListNew(&c2);
-  addDataToHead(&testersP2, &c3);
-  setPaper(&p2, "p2", &testersP2);
+  LinkedList *takersP2 = linkListNew(&c2);
+  addDataToHead(&takersP2, &c3);
+  setPaper(&p2, "p2", takersP2);
   
-  LinkedList *testersP3 = linkListNew(&c3);
-  addDataToHead(&testersP3, &c4);
-  setPaper(&p3, "p3", &testersP3);
+  LinkedList *takersP3 = linkListNew(&c3);
+  addDataToHead(&takersP3, &c4);
+  setPaper(&p3, "p3", takersP3);
   
-  LinkedList *testersP4 = linkListNew(&c4);
-  addDataToHead(&testersP4, &c5);
-  setPaper(&p4, "p4", &testersP4);
+  LinkedList *takersP4 = linkListNew(&c4);
+  addDataToHead(&takersP4, &c5);
+  setPaper(&p4, "p4", takersP4);
   
-  LinkedList *testersP5 = linkListNew(&c5);
-  addDataToHead(&testersP5, &c6);
-  setPaper(&p5, "p5", &testersP5);
+  LinkedList *takersP5 = linkListNew(&c7);
+  setPaper(&p4, "p4", takersP4);
   
-  LinkedList *testersP6 = linkListNew(&c6);
-  addDataToHead(&testersP6, &c1);
-  setPaper(&p6, "p6", &testersP6);
+  LinkedList *takersP6 = linkListNew(&c2);
+  addDataToHead(&takersP6, &c3);
+  addDataToHead(&takersP6, &c4);
+  setPaper(&p6, "p6", takersP6);
   
+
+/**
+ *                                 PaperList
+ * Papers                  :   p1              p2              p3           p4          p5        p6
+ * programmes taking       : c1,c2            c2,c3          c3,c4        c4,c5        c7       c2,c3,c4
+*/
 }
 
 void tearDown(void){}
 
+/**
+ *                                 session
+ * Papers                  :   p1              p3            p5 
+ * programmes taking       : c1,c2            c3,c4         c7      
+*/
 void test_calFitnessScore_given_session_has_papers_p1_p3_p5_which_does_not_cause_conflict_in_programmes_should_get_0(void)
 {
 	LinkedList *papers = linkListNew(&p1);
   addDataToHead(&papers, &p3);
   addDataToHead(&papers, &p5);
-  
   session.papers = papers;
   
   calFitnessScore(&session);
   TEST_ASSERT_EQUAL(0,session.fitnessScore);
 }
 
-
-void test_calFitnessScore_given_session_has_papers_p1_p2_p5_which_does_not_cause_1conflict_in_programmes_should_get_1(void)
+/**
+ *                                 session
+ * Papers                  :   p1              p2              p5
+ * programmes taking       : c1,c2            c2,c3            c7
+ *                              -             -
+ *                            ( 1 conflict, p1 conflict with p2)
+*/
+void test_calFitnessScore_given_session_has_papers_p1_p2_p5_which_cause_1conflict_in_programmes_should_get_1(void)
 {
 	LinkedList *papers = linkListNew(&p1);
-  addDataToHead(&papers, &p3);
+  addDataToHead(&papers, &p2);
   addDataToHead(&papers, &p5);
-  
   session.papers = papers;
   
   calFitnessScore(&session);
   TEST_ASSERT_EQUAL(1,session.fitnessScore);
 }
+
+/**
+ *                                 session
+ * Papers                  :   p1              p2              p3
+ * programmes taking       : c1,c2            c2,c3         c3,c4
+ *   p1 got 1 conflict           -             -
+ *   p2 got 1 conflict                           -          -
+ *                            ( 1+1 = 2 )
+*/
+void test_calFitnessScore_given_session_has_papers_p1_p2_p3_which_cause_2conflict_in_programmes_should_get_2(void)
+{
+	LinkedList *papers = linkListNew(&p1);
+  addDataToHead(&papers, &p2);
+  addDataToHead(&papers, &p3);
+  session.papers = papers;
+  
+  calFitnessScore(&session);
+  TEST_ASSERT_EQUAL(2,session.fitnessScore);
+}
+
+/**
+ *                                 session
+ * Papers                  :   p1              p2              p3          p4            
+ * programmes taking       : c1,c2            c2,c3         c3,c4         c4,c5         
+ *   p1 got 1 conflict          -             -                                         
+ *   p2 got 1 conflict                           -          -                          
+ *   p3 got 1 conflict                                          -         -                   
+ *                            ( 1+1+1 = 3 )
+*/
+void test_calFitnessScore_given_session_has_papers_p1_p2_p3_p4_which_cause_3conflict_in_programmes_should_get_3(void)
+{
+	LinkedList *papers = linkListNew(&p1);
+  addDataToHead(&papers, &p2);
+  addDataToHead(&papers, &p3);
+  addDataToHead(&papers, &p4);
+  session.papers = papers;
+  
+  calFitnessScore(&session);
+  TEST_ASSERT_EQUAL(3,session.fitnessScore);
+}
+
+/**
+ *                                 session
+ * Papers                  :   p1              p2              p3          p4            p6
+ * programmes taking       : c1,c2            c2,c3         c3,c4         c4,c5         c2,c3,c4
+ *   p1 got 2 conflict          -             -                                         -
+ *   p2 got 3 conflict                           -          -   -                          -   -
+ *   p3 got 2 conflict                                          -         -                    -
+ *                            ( 2+3+2 = 7 )
+*/
+void test_calFitnessScore_given_session_has_papers_p1_p2_p3_p4_p6_which_cause_7conflict_in_programmes_should_get_7(void)
+{
+	LinkedList *papers = linkListNew(&p1);
+  addDataToHead(&papers, &p2);
+  addDataToHead(&papers, &p3);
+  addDataToHead(&papers, &p4);
+  addDataToHead(&papers, &p6);
+  session.papers = papers;
+  
+  calFitnessScore(&session);
+  TEST_ASSERT_EQUAL(7,session.fitnessScore);
+}
+
